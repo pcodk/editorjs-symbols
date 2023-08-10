443c19940734646911379cb0b7a515901b71828c
Dimitar Dimitrov <ddi@peytz.dk>
2023-08-10 15:11:56 +0300

    MYL-297 - add katex first attempt

--- index.ts
+++ index.ts
@@ -20,7 +20,7 @@ class InlineGreekLetters {
         this.state = false;
         this.button = null;
         this.icon = '&Phi;'; // '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2"><path d="M17.143 12.5c0-3.498-2.845-6.333-6.357-6.333-3.513 0-6.358 2.835-6.358 6.333s2.845 6.333 6.358 6.333c3.512 0 6.357-2.835 6.357-6.333zm-4.035-3.083l-1.607 3.085-1.608-3.085h3.215zm3.143 5.583c0 1.63-1.326 2.958-2.964 2.958h-2.572v-1.5h2.572c.81 0 1.464-.654 1.464-1.458v-.084c0-.64-.418-1.18-.987-1.376l.662-1.538h-.935l-.618 1.437h-1.607v-1.437h1.607l-.618-1.437h.95l.662 1.538c-.568.196-.968.736-.968 1.376v.084c0 .804.654 1.458 1.464 1.458h2.572v1.5h-2.572c-1.638 0-2.964-1.328-2.964-2.958h-.015v-.084c0-.644.415-1.193.99-1.39l-.683-1.587h1.015l.638 1.475h1.571v-1.475h-1.572l.638-1.475h1.015l-.683 1.587c.576.196.99.746.99 1.39v.084h.016zm-6.358-7.333c2.078 0 3.767 1.682 3.767 3.75s-1.689 3.75-3.767 3.75c-2.078 0-3.767-1.682-3.767-3.75s1.689-3.75 3.767-3.75zm0 1.5c1.183 0 2.143.957 2.143 2.25s-.96 2.25-2.143 2.25c-1.183 0-2.143-.957-2.143-2.25s.96-2.25 2.143-2.25z"/></svg>';
-        this.tag = 'span';
+        this.tag = 'SPAN';
         this.iconClasses = {
             base: this.api.styles.inlineToolButton,
             active: this.api.styles.inlineToolButtonActive
@@ -63,6 +63,7 @@ class InlineGreekLetters {
     surround(range: any) {
         console.log('entering surround');
         const selectedText = window.getSelection()?.toString() + "";
+        console.log(window.getSelection());
         console.log(this.api.selection);
         let termWrapper = this.api.selection.findParentTag(this.tag, InlineGreekLetters.CSS);
         console.log(termWrapper)
