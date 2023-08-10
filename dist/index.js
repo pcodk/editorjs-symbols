4c932097f64c9f399a7c106bfe2623ff34405a79
Dimitar Dimitrov <ddi@peytz.dk>
2023-08-10 11:49:53 +0300

    MYL-297 - add katex first attempt

--- dist/index.js
+++ dist/index.js
@@ -17,7 +17,16 @@ class InlineGreekLetters {
         this.button = document.createElement('button');
         this.state = false;
         this.icon = '&Phi;'; // '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2"><path d="M17.143 12.5c0-3.498-2.845-6.333-6.357-6.333-3.513 0-6.358 2.835-6.358 6.333s2.845 6.333 6.358 6.333c3.512 0 6.357-2.835 6.357-6.333zm-4.035-3.083l-1.607 3.085-1.608-3.085h3.215zm3.143 5.583c0 1.63-1.326 2.958-2.964 2.958h-2.572v-1.5h2.572c.81 0 1.464-.654 1.464-1.458v-.084c0-.64-.418-1.18-.987-1.376l.662-1.538h-.935l-.618 1.437h-1.607v-1.437h1.607l-.618-1.437h.95l.662 1.538c-.568.196-.968.736-.968 1.376v.084c0 .804.654 1.458 1.464 1.458h2.572v1.5h-2.572c-1.638 0-2.964-1.328-2.964-2.958h-.015v-.084c0-.644.415-1.193.99-1.39l-.683-1.587h1.015l.638 1.475h1.571v-1.475h-1.572l.638-1.475h1.015l-.683 1.587c.576.196.99.746.99 1.39v.084h.016zm-6.358-7.333c2.078 0 3.767 1.682 3.767 3.75s-1.689 3.75-3.767 3.75c-2.078 0-3.767-1.682-3.767-3.75s1.689-3.75 3.767-3.75zm0 1.5c1.183 0 2.143.957 2.143 2.25s-.96 2.25-2.143 2.25c-1.183 0-2.143-.957-2.143-2.25s.96-2.25 2.143-2.25z"/></svg>';
+        this.tag = 'span';
+        this.iconClasses = {
+            base: this.api.styles.inlineToolButton,
+            active: this.api.styles.inlineToolButtonActive
+        };
     }
+    static get CSS() {
+        return 'cdx-latex-render';
+    }
+    ;
     static get sanitize() {
         return {
             customkatex: function (el) {
@@ -35,21 +44,24 @@ class InlineGreekLetters {
     checkState(selection) {
         const text = selection.anchorNode;
         if (!text) {
-            console.log('in !text');
             return;
         }
-        const anchorElement = text instanceof Element ? text : text.parentElement;
-        if (anchorElement) {
-            console.log('in achromelement');
-            this.state = !!anchorElement.closest('MARK');
-        }
+        const termTag = this.api.selection.findParentTag(this.tag, InlineGreekLetters.CSS);
+        this.button.classList.toggle(this.iconClasses.active, !!termTag);
     }
-    surround() {
-        // this.button.classList.add(this.api.styles.inlineToolButton);
-        // this.button.innerHTML = this.config.buttonIcon;
-        var _a, _b, _c;
-        // this.button.addEventListener('click', () => {
+    surround(range) {
+        var _a, _b;
         const selectedText = ((_a = window.getSelection()) === null || _a === void 0 ? void 0 : _a.toString()) + "";
+        let termWrapper = this.api.selection.findParentTag(this.tag, InlineGreekLetters.CSS);
+        /**
+         * If start or end of selection is in the highlighted block
+         */
+        if (termWrapper) {
+            this.unwrap(termWrapper);
+        }
+        else {
+            this.wrap(range);
+        }
         let result = this.createGreekLetter(selectedText);
         const toInsert = document.createElement('span');
         if (result === null) {
@@ -72,18 +84,75 @@ class InlineGreekLetters {
                 katexResult.innerHTML = katex_1.default.renderToString(selectedText);
                 katexResult.style.display = 'block';
             }
-            const range = (_b = window.getSelection()) === null || _b === void 0 ? void 0 : _b.getRangeAt(0);
+            /*
+            const range = window.getSelection()?.getRangeAt(0);
             toInsert.innerText = selectedText;
-            range === null || range === void 0 ? void 0 : range.deleteContents();
-            range === null || range === void 0 ? void 0 : range.insertNode(toInsert);
+            range?.deleteContents();
+            range?.insertNode(toInsert);
+
+             */
         }
         else {
             if (result.textContent) {
                 toInsert.innerText = result.textContent;
             }
-            const range = (_c = window.getSelection()) === null || _c === void 0 ? void 0 : _c.getRangeAt(0);
+            const range = (_b = window.getSelection()) === null || _b === void 0 ? void 0 : _b.getRangeAt(0);
             range === null || range === void 0 ? void 0 : range.deleteContents();
             range === null || range === void 0 ? void 0 : range.insertNode(toInsert);
+        }
+    }
+    /**
+     * Wrap selection with term-tag
+     *
+     * @param {Range} range - selected fragment
+     */
+    wrap(range) {
+        /**
+         * Create a wrapper for highlighting
+         */
+        let supElement = document.createElement(this.tag);
+        supElement.classList.add(InlineGreekLetters.CSS);
+        /**
+         * SurroundContent throws an error if the Range splits a non-Text node with only one of its boundary points
+         * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/Range/surroundContents}
+         *
+         * // range.surroundContents(sup);
+         */
+        supElement.appendChild(range.extractContents());
+        range.insertNode(supElement);
+        /**
+         * Expand (add) selection to highlighted block
+         */
+        this.api.selection.expandToTag(supElement);
+    }
+    /**
+     * Unwrap term-tag
+     *
+     * @param {HTMLElement} termWrapper - term wrapper tag
+     */
+    unwrap(termWrapper) {
+        var _a;
+        /**
+         * Expand selection to all term-tag
+         */
+        this.api.selection.expandToTag(termWrapper);
+        let sel = window.getSelection();
+        let range = sel === null || sel === void 0 ? void 0 : sel.getRangeAt(0);
+        let unwrappedContent = range === null || range === void 0 ? void 0 : range.extractContents();
+        /**
+         * Remove empty term-tag
+         */
+        (_a = termWrapper.parentNode) === null || _a === void 0 ? void 0 : _a.removeChild(termWrapper);
+        if (range && unwrappedContent) {
+            /**
+             * Insert extracted content
+             */
+            range === void 0 ? void 0 : range.insertNode(unwrappedContent);
+            /**
+             * Restore selection
+             */
+            sel === null || sel === void 0 ? void 0 : sel.removeAllRanges();
+            sel === null || sel === void 0 ? void 0 : sel.addRange(range);
         }
     }
     render() {
