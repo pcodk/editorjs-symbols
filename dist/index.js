054699dd64d88cf7d494c608eaa41fcf6619cc40
Dimitar Dimitrov <ddi@peytz.dk>
2023-08-11 11:22:11 +0300

    MYL-297 - add katex first attempt

--- dist/index.js
+++ dist/index.js
@@ -12,7 +12,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const katex_1 = __importDefault(require("katex"));
 // @ts-ignore
 require('./index.css').toString();
-class InlineGreekLetters {
+class Symbols {
     constructor({ config, api }) {
         this.config = config;
         this.api = api;
@@ -32,7 +32,7 @@ class InlineGreekLetters {
     ;
     static get toolbox() {
         return {
-            title: 'Greek Letters',
+            title: 'Greek letters and Katex',
             icon: '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2"><path d="M17.143 12.5c0-3.498-2.845-6.333-6.357-6.333-3.513 0-6.358 2.835-6.358 6.333s2.845 6.333 6.358 6.333c3.512 0 6.357-2.835 6.357-6.333zm-4.035-3.083l-1.607 3.085-1.608-3.085h3.215zm3.143 5.583c0 1.63-1.326 2.958-2.964 2.958h-2.572v-1.5h2.572c.81 0 1.464-.654 1.464-1.458v-.084c0-.64-.418-1.18-.987-1.376l.662-1.538h-.935l-.618 1.437h-1.607v-1.437h1.607l-.618-1.437h.95l.662 1.538c-.568.196-.968.736-.968 1.376v.084c0 .804.654 1.458 1.464 1.458h2.572v1.5h-2.572c-1.638 0-2.964-1.328-2.964-2.958h-.015v-.084c0-.644.415-1.193.99-1.39l-.683-1.587h1.015l.638 1.475h1.571v-1.475h-1.572l.638-1.475h1.015l-.683 1.587c.576.196.99.746.99 1.39v.084h.016zm-6.358-7.333c2.078 0 3.767 1.682 3.767 3.75s-1.689 3.75-3.767 3.75c-2.078 0-3.767-1.682-3.767-3.75s1.689-3.75 3.767-3.75zm0 1.5c1.183 0 2.143.957 2.143 2.25s-.96 2.25-2.143 2.25c-1.183 0-2.143-.957-2.143-2.25s.96-2.25 2.143-2.25z"/></svg>',
         };
     }
@@ -42,7 +42,7 @@ class InlineGreekLetters {
         if (!text) {
             return;
         }
-        const termTag = this.api.selection.findParentTag(this.tag, InlineGreekLetters.CSS);
+        const termTag = this.api.selection.findParentTag(this.tag, Symbols.CSS);
         const actionsElement = document.getElementById(this.actionsElementId);
         if (!actionsElement) {
             return;
@@ -67,7 +67,7 @@ class InlineGreekLetters {
     surround(range) {
         var _a;
         const selectedText = ((_a = window.getSelection()) === null || _a === void 0 ? void 0 : _a.toString()) + "";
-        let termWrapper = this.api.selection.findParentTag(this.tag, InlineGreekLetters.CSS);
+        let termWrapper = this.api.selection.findParentTag(this.tag, Symbols.CSS);
         if (termWrapper) {
             this.unwrap(termWrapper);
             this.clearActionsContent();
@@ -98,17 +98,10 @@ class InlineGreekLetters {
         }
         element.style.display = 'block';
     }
-    /**
-     * Wrap selection with term-tag
-     *
-     * @param {Range} range - selected fragment
-     */
     wrap(range) {
-        /**
-         * Create a wrapper for highlighting
-         */
+        //Create a wrapper for highlighting
         let supElement = document.createElement(this.tag);
-        supElement.classList.add(InlineGreekLetters.CSS);
+        supElement.classList.add(Symbols.CSS);
         /**
          * SurroundContent throws an error if the Range splits a non-Text node with only one of its boundary points
          * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/Range/surroundContents}
@@ -117,37 +110,22 @@ class InlineGreekLetters {
          */
         supElement.appendChild(range.extractContents());
         range.insertNode(supElement);
-        /**
-         * Expand (add) selection to highlighted block
-         */
+        // Expand (add) selection to highlighted block
         this.api.selection.expandToTag(supElement);
     }
-    /**
-     * Unwrap term-tag
-     *
-     * @param {HTMLElement} termWrapper - term wrapper tag
-     */
     unwrap(termWrapper) {
         var _a;
-        /**
-         * Expand selection to all term-tag
-         */
+        //Expand selection to all term-tag
         this.api.selection.expandToTag(termWrapper);
         let sel = window.getSelection();
         let range = sel === null || sel === void 0 ? void 0 : sel.getRangeAt(0);
         let unwrappedContent = range === null || range === void 0 ? void 0 : range.extractContents();
-        /**
-         * Remove empty term-tag
-         */
+        // Remove empty term-tag
         (_a = termWrapper.parentNode) === null || _a === void 0 ? void 0 : _a.removeChild(termWrapper);
         if (range && unwrappedContent) {
-            /**
-             * Insert extracted content
-             */
+            //Insert extracted content
             range === null || range === void 0 ? void 0 : range.insertNode(unwrappedContent);
-            /**
-             * Restore selection
-             */
+            // Restore selection
             sel === null || sel === void 0 ? void 0 : sel.removeAllRanges();
             sel === null || sel === void 0 ? void 0 : sel.addRange(range);
         }
@@ -160,10 +138,10 @@ class InlineGreekLetters {
         return this.button;
     }
     renderActions() {
-        const katexResult = document.createElement('span');
-        katexResult.setAttribute("id", this.actionsElementId);
-        katexResult.style.display = 'none';
-        return katexResult;
+        const element = document.createElement('span');
+        element.setAttribute("id", this.actionsElementId);
+        element.style.display = 'none';
+        return element;
     }
     createGreekLetter(letter) {
         const greekLetters = {
@@ -232,4 +210,4 @@ class InlineGreekLetters {
         return true;
     }
 }
-exports.default = InlineGreekLetters;
+exports.default = Symbols;
--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ import katex from 'katex';
 // @ts-ignore
 require('./index.css').toString();
 
-class InlineGreekLetters {
+class Symbols {
     api;
     config;
     button: HTMLButtonElement|null;
@@ -37,7 +37,7 @@ class InlineGreekLetters {
 
     static get toolbox() {
         return {
-            title: 'Greek Letters',
+            title: 'Greek letters and Katex',
             icon: '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2"><path d="M17.143 12.5c0-3.498-2.845-6.333-6.357-6.333-3.513 0-6.358 2.835-6.358 6.333s2.845 6.333 6.358 6.333c3.512 0 6.357-2.835 6.357-6.333zm-4.035-3.083l-1.607 3.085-1.608-3.085h3.215zm3.143 5.583c0 1.63-1.326 2.958-2.964 2.958h-2.572v-1.5h2.572c.81 0 1.464-.654 1.464-1.458v-.084c0-.64-.418-1.18-.987-1.376l.662-1.538h-.935l-.618 1.437h-1.607v-1.437h1.607l-.618-1.437h.95l.662 1.538c-.568.196-.968.736-.968 1.376v.084c0 .804.654 1.458 1.464 1.458h2.572v1.5h-2.572c-1.638 0-2.964-1.328-2.964-2.958h-.015v-.084c0-.644.415-1.193.99-1.39l-.683-1.587h1.015l.638 1.475h1.571v-1.475h-1.572l.638-1.475h1.015l-.683 1.587c.576.196.99.746.99 1.39v.084h.016zm-6.358-7.333c2.078 0 3.767 1.682 3.767 3.75s-1.689 3.75-3.767 3.75c-2.078 0-3.767-1.682-3.767-3.75s1.689-3.75 3.767-3.75zm0 1.5c1.183 0 2.143.957 2.143 2.25s-.96 2.25-2.143 2.25c-1.183 0-2.143-.957-2.143-2.25s.96-2.25 2.143-2.25z"/></svg>',
         };
     }
@@ -49,7 +49,7 @@ class InlineGreekLetters {
             return;
         }
 
-        const termTag = this.api.selection.findParentTag(this.tag, InlineGreekLetters.CSS);
+        const termTag = this.api.selection.findParentTag(this.tag, Symbols.CSS);
         const actionsElement = document.getElementById(this.actionsElementId);
         if (!actionsElement) {
             return;
@@ -75,7 +75,7 @@ class InlineGreekLetters {
     surround(range: any) {
         const selectedText = window.getSelection()?.toString() + "";
 
-        let termWrapper = this.api.selection.findParentTag(this.tag, InlineGreekLetters.CSS);
+        let termWrapper = this.api.selection.findParentTag(this.tag, Symbols.CSS);
 
         if (termWrapper) {
             this.unwrap(termWrapper);
@@ -111,18 +111,11 @@ class InlineGreekLetters {
         element.style.display = 'block';
     }
 
-    /**
-     * Wrap selection with term-tag
-     *
-     * @param {Range} range - selected fragment
-     */
     wrap(range: Range) {
-        /**
-         * Create a wrapper for highlighting
-         */
+        //Create a wrapper for highlighting
         let supElement = document.createElement(this.tag);
 
-        supElement.classList.add(InlineGreekLetters.CSS);
+        supElement.classList.add(Symbols.CSS);
 
         /**
          * SurroundContent throws an error if the Range splits a non-Text node with only one of its boundary points
@@ -133,40 +126,25 @@ class InlineGreekLetters {
         supElement.appendChild(range.extractContents());
         range.insertNode(supElement);
 
-        /**
-         * Expand (add) selection to highlighted block
-         */
+        // Expand (add) selection to highlighted block
         this.api.selection.expandToTag(supElement);
     }
 
-    /**
-     * Unwrap term-tag
-     *
-     * @param {HTMLElement} termWrapper - term wrapper tag
-     */
     unwrap(termWrapper: HTMLElement) {
-        /**
-         * Expand selection to all term-tag
-         */
+        //Expand selection to all term-tag
         this.api.selection.expandToTag(termWrapper);
 
         let sel = window.getSelection();
         let range = sel?.getRangeAt(0);
 
         let unwrappedContent = range?.extractContents();
-        /**
-         * Remove empty term-tag
-         */
+        // Remove empty term-tag
         termWrapper.parentNode?.removeChild(termWrapper);
         if (range && unwrappedContent) {
-            /**
-             * Insert extracted content
-             */
+            //Insert extracted content
             range?.insertNode(unwrappedContent);
 
-            /**
-             * Restore selection
-             */
+            // Restore selection
             sel?.removeAllRanges();
             sel?.addRange(range);
         }
@@ -182,10 +160,10 @@ class InlineGreekLetters {
     }
 
     renderActions() {
-        const katexResult = document.createElement('span');
-        katexResult.setAttribute("id", this.actionsElementId)
-        katexResult.style.display = 'none';
-        return katexResult;
+        const element = document.createElement('span');
+        element.setAttribute("id", this.actionsElementId)
+        element.style.display = 'none';
+        return element;
     }
 
     createGreekLetter(letter: string) {
@@ -262,4 +240,4 @@ class InlineGreekLetters {
     }
 }
 
-export default InlineGreekLetters;
+export default Symbols;
